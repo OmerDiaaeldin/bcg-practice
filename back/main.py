931597503fc95447f74013743d74fdc44ec96d94
AchouakG <Ak.Guennoun@gmail.com>
2025-04-06 19:29:58 +0100

    backend 3

--- back/main.py
+++ back/main.py
@@ -1,6 +1,8 @@
 from typing import Union
 from pydantic import BaseModel
 from fastapi import APIRouter, FastAPI
+from src.prisma import prisma
+
 
 app = FastAPI()
 router = APIRouter()
@@ -14,33 +16,46 @@ class Student(BaseModel):
     course: str
 
 
-@router.post("/Students/addStudents/")
+@router.get("/Student/viewStudent/")
+async def view(item: Student):
+    
+@router.post("/Student/addStudent/")
 async def create_item(item: Student):
-    Student_dict = item.dict()
-    if item.enrollment :
-        first_name= item.first_name
-        last_name= item.last_name
-        email= item.email
-        number= item.number
-        course=item.course
-        Student_dict.append({"first_name": first_name}, {"last_name": last_name}, {"email": email}, {"number": number}, {"course": course})
-    return Student_dict
+    created = await Student.prisma().create(
+        {
+            "email": item.email,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "number": item.number,
+            "course": item.course,
+        }
+    )
+    return created
 
 
 
-@router.put("/Students/EditStudents/")
+@router.put("/Student/EditStudent/")
 async def edit_item(item_id: int, item: Student):
-    StudentOnCourse = []
-    for student in StudentOnCourse:
-        if item.enrollment :
-            name= item.name
-            email= item.email
-            number= item.number
-            course=item.course
-            student.update({"name": name}, {"email": email}, {"number": number}, {"course": course})
-        return student
+    updated = await Student.prisma().update(
+        {
+            "email": item.email,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "number": item.number,
+            "course": item.course,
+        }
+    )
+    return updated
 
 
+# @router.delete("/Student/DeleteStudent/")
+# async def delete_item(item_id: int):
+    
+#     student = await Student.prisma().delete(
+#         data={
+
+#         },
+#     )
 
 
 
