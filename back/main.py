42d85a1ac3cc522d7d4408593a3a14c6fdb92c8b
AchouakG <Ak.Guennoun@gmail.com>
2025-04-06 18:29:49 +0100

    backend part

--- back/main.py
+++ back/main.py
@@ -1,15 +1,41 @@
 from typing import Union
-
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import APIRouter, FastAPI
 
 app = FastAPI()
+router = APIRouter()
+
+class Item(BaseModel):
+    name: str
+    email: str
+    enrollment: bool
+    number: int
+    course: str
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@router.post("/Students/addStudents/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.enrollment :
+        name= item.name
+        email= item.email
+        number= item.number
+        course=item.course
+        item_dict.update({"name": name}, {"email": email}, {"number": number}, {"course": course})
+    return item_dict
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@router.put("/Students/EditStudents/")
+async def edit_item(item_id: int, item: Item):
+    for student in Students:
+        if item.enrollment :
+            name= item.name
+            email= item.email
+            number= item.number
+            course=item.course
+            item_dict.update({"name": name}, {"email": email}, {"number": number}, {"course": course})
+        return item_dict
+
+
+app.include_router(router)
